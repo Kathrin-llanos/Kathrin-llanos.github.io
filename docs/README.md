c892029cfb7344e353986ee0ecc9360b35d68290
Kathrin-llanos <katherinllanos617@gmail.com>
2024-07-11 17:25:43 -0500

    Add files via upload

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# aplicacion-mopvil-ka
+# kathrin-llanos.github.io
+# Kathrin-llanos.github.io
+# Kathrin-llanos.github.io
---
+++ docs/script.js
@@ -0,0 +1,3 @@
+document.getElementById('clickMe').addEventListener('click', function() {
+    document.getElementById('message').textContent = '¡Has hecho clic en el botón!';
+});
